ce58da842a3b5f398fba45c3b2f5d5a485d3370e
dorobonneko <732679753@qq.com>
2017-08-15 18:59:28 +0800

    Initial commit

---
+++ app/src/main/java/com/moe/yaohuo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.moe.yaohuo;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
